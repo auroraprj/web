d8b7e72b3d51f7c4480b5469704f6560d84a5261
Jose Carmona <j.carmona.n@gmail.com>
2017-02-14 00:46:10 +0100

    primera tentativa de soporte multiidioma

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # auroraprj/web
 
-El objetivo principal de este proyecto es dar soporte en la web al Catálogo de investigaciones
-en Cáncer Infantil.
+El objetivo principal de este proyecto es dar soporte en la web al Catálogo de Investigaciones en Cáncer Infantil.
 
 El site contendrá:
 * El propio Catálogo de investigaciones en Cáncer Infantil.
@@ -27,30 +26,34 @@ en otros entornos mediante `composer` (por ejemplo en Docker).
  - un conjunto de módulos específicos para auroraprj (en el futuro).
  - un aspecto o theme (actualmente Bootstrap).
  - un profile de instalación drupal que empaqueta todo el conjunto y permite una instalación homogenea.
+- web:
+ - Soporte multidioma.
 
 ### Setup
 
-* Descargamos máquina virtual bitnami con stack drupal: https://bitnami.com/stack/drupal
-* Nos logamos con el usuario bitnami y clonamos el Proyecto: `git clone https://github.com/auroraprj/web.git`
-* El módulo `rest` necesita activar la variable `always_populate_raw_post_data=-1` en `php.ini`
-* Lanzamos inicialización: `./web/tools/aurora_init.sh`
+- Descargamos máquina virtual bitnami con stack drupal: https://bitnami.com/stack/drupal
+- Nos logamos con el usuario bitnami y clonamos el Proyecto: `git clone https://github.com/auroraprj/web.git`
+- Lanzamos inicialización: `./web/tools/aurora_init.sh`.
+- No olvides tomar tona de la clave del usuario `admin` autogenerada en la instalación.
 
 ### Notas
 
-* Asumimos que la clonación se hace en el directorio `$HOME/web`
-* El proceso de init activa el servicio `ssh`. Por favor, téngalo en cuenta a efectos de seguridad.
-* El proceso init pedirá clave dado que algunas acciones necesita ejecución con `sudo`
+- Asumimos que la clonación se hace en el directorio `$HOME/web`
+- El proceso aurora_init.sh:
+ - pedirá clave dado que algunas acciones necesita ejecución con privilegios de root (`sudo`).
+ - activa el servicio `ssh`. Por favor, téngalo en cuenta a efectos de seguridad.
+ - activa la variable `always_populate_raw_post_data=-1` en `php.ini` dado que el módulo `rest` lo necesita.
 
 ### Contenido para tests
 
-* Script de importación `tools/import_data_test.sh`
- * Página con todos los elementos disponibles.
- * Investigación.
+- Script de importación `tools/import_data_test.sh <clave_admin>`
+ - Página con todos los elementos disponibles.
+ - Investigación.
 
 ### Directorios
 
-* `media` --> Elementos multimedia necesarios en la instalación.
-* `test/content` --> Contenido para test.
-* `tools` --> Herramientas para manejo de la instalación: inicialización, exportación, migración, etc.
-* `profile/auroraprj` --> Profile de instalación del site.
-* `profile/auroraprj/config/install` --> Configuración inicial.
+- `media` --> Elementos multimedia necesarios en la instalación.
+- `test/content` --> Contenido para test.
+- `tools` --> Herramientas para manejo de la instalación: inicialización, exportación, migración, etc.
+- `profile/auroraprj` --> Profile de instalación del site.
+- `profile/auroraprj/config/install` --> Configuración inicial.
--- tools/aurora_init.sh
+++ tools/aurora_init.sh
@@ -1,50 +1,133 @@
+#!/bin/bash
+
+getopt --test > /dev/null
+if [[ $? -ne 4 ]]; then
+    echo "opss!! `getopt --test` failed in this environment"
+    exit 1
+fi
+
+init_bitnami=1
+reinstall=0
+init_drupal=1
+
+SHORT=brd
+LONG=skipbitnami,reinstall,skipdrupal
+
+# -temporarily store output to be able to check for errors
+# -activate advanced mode getopt quoting e.g. via “--options”
+# -pass arguments only via   -- "$@"   to separate them correctly
+PARSED=`getopt --options $SHORT --longoptions $LONG --name "$0" -- "$@"`
+if [[ $? -ne 0 ]]; then
+    # e.g. $? == 1
+    #  then getopt has complained about wrong arguments to stdout
+    exit 2
+fi
+
+# use eval with "$PARSED" to properly handle the quoting
+eval set -- "$PARSED"
+
+# now enjoy the options in order and nicely split until we see --
+while true; do
+    case "$1" in
+        -b|--skipbitnami)
+            init_bitnami=0
+            shift
+            ;;
+        -r|--reinstall)
+            reinstall=1
+            shift
+            ;;
+        -d|--skipdrupal)
+            init_drupal=0
+            shift
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            echo "Opps! error al procesars opciones"
+            exit 3
+            ;;
+    esac
+done
+
 #-- entorno
 . $HOME/web/tools/aurora_env.sh
 
-#-- activamos ssh
-sudo mv /etc/init/ssh.conf.back /etc/init/ssh.conf
-sudo start ssh
+if [ $init_bitnami == 1 ]
+then
+  #-- activamos ssh
+  sudo mv /etc/init/ssh.conf.back /etc/init/ssh.conf
+  sudo start ssh
 
-#-- eliminamos banner de bitnami
-sudo /opt/bitnami/apps/drupal/bnconfig --disable_banner 1
+  #-- eliminamos banner de bitnami
+  sudo /opt/bitnami/apps/drupal/bnconfig --disable_banner 1
 
-#-- nos aseguramos que tenemo el parámetro always_populate_raw_post_data = -1 en php.ini
-always_populate='always_populate_raw_post_data = -1'
+  #-- nos aseguramos que tenemo el parámetro always_populate_raw_post_data = -1 en php.ini
+  always_populate='always_populate_raw_post_data = -1'
 
-#-- averiguamos la ubicación de php.ini
-phpini=`php --ini | egrep --only-matching '/.*/php.ini'`
+  #-- averiguamos la ubicación de php.ini
+  phpini=`php --ini | egrep --only-matching '/.*/php.ini'`
 
-#-- comprobamos que está activado el parámetro y lo activamos si fuera necesario
-egrep --silent "^[^; ]*$always_populate" $phpini || echo $always_populate >> $phpini
+  #-- comprobamos que está activado el parámetro y lo activamos si fuera necesario
+  egrep --silent "^[^; ]*$always_populate" $phpini || echo $always_populate >> $phpini
 
-# posiblemente la forma de actualizar el parámetro no es muy elegante, ya que soltamos
-# el parámetro en la última linea, fuera de su sección, pero parece que funciona
-# adecuadamente. Siempre se tiene la opción de incluir manualmente dicho parámetro,
-# con lo que el script no actuará
+  # posiblemente la forma de actualizar el parámetro no es muy elegante, ya que soltamos
+  # el parámetro en la última linea, fuera de su sección, pero parece que funciona
+  # adecuadamente. Siempre se tiene la opción de incluir manualmente dicho parámetro,
+  # con lo que el script no actuará
 
-#-- relanzamos apache
-sudo /opt/bitnami/ctlscript.sh restart apache
+  #-- relanzamos apache
+  sudo /opt/bitnami/ctlscript.sh restart apache
+fi
 
-#-- vamos con drupal. Usaremos drush para operar
-cd $drupal
+if [ $init_drupal == 1 ]
+then
 
-#-- copiamos profile
-cp -R $git/profile/auroraprj $drupal/profiles/
+  #-- vamos con drupal. Usaremos drush para operar
+  cd $drupal
 
-#-- descargamos Bootstrap theme
-drush pm-download bootstrap
+  if [ $reinstall == 1 ]
+  then
+    rm -R $drupal/profiles/auroraprj/
+  fi
 
-#-- descargamos módulo RESTui
-drush pm-download restui
+  #-- copiamos profile
+  cp -R $git/profile/auroraprj $drupal/profiles/
 
-#-- necesario para instalación
-chmod u+w ./sites/default/settings.php
+  #-- descargamos Bootstrap theme
+  drush -y pm-download bootstrap
 
-#-- instalamos el site auroraprj
-drush -y site-install auroraprj
+  #-- descargamos módulo RESTui
+  drush -y pm-download restui
 
-#-- copiamos logo que se usa en theme
-cp $git/media/logo_aurora_grises_80.png sites/default/files
+  #-- descargamos módulo drush para automatizar operaciones con language
+  drush -y pm-download drush_language
 
-#-- cache
-sudo -u daemon -g daemon drush cache-rebuild
+  #-- necesario para instalación
+  chmod u+w ./sites/default/settings.php
+
+  #-- instalamos el site auroraprj
+  drush -y site-install auroraprj
+
+  #-- copiamos logo que se usa en theme
+  cp $git/media/logo_aurora_grises_80.png sites/default/files
+
+  #-- acciones post-instalación
+
+  #-- nombre del sitio
+  drush -y config-set system.site name Auroraprj
+
+  #-- nombre del sitio
+  drush -y config-set system.site slogan 'La investigación es la clave'
+
+  #-- lenguaje por defecto en español
+  drush -y config-set system.site default_langcode es
+
+  #-- actualizamos las traducciones
+  drush locale-update
+
+  #-- cache
+  sudo -u daemon -g daemon drush cache-rebuild
+fi
