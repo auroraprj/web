191a0fd6e4c1150d17fd472f73a5a9cbdd618bbb
Jose Carmona <j.carmona.n@gmail.com>
2017-06-13 18:57:57 +0200

    creo estructura para tema aurora_theme como subtema de bootstrap

--- tools/aurora_init.sh
+++ tools/aurora_init.sh
@@ -156,10 +156,6 @@ then
 
     #-- activamos drupal-extension
     composer require --dev drupal/drupal-extension:~3.0
-
-    #-- aplicamos parche a drupal-extension (ver pull-request #369 de drupal-extension)
-    cd $drupal/vendor/drupal/drupal-extension
-    curl https://patch-diff.githubusercontent.com/raw/jhedstrom/drupalextension/pull/369.diff | patch -p1 --forward
   fi
 
   #-- actualizamos las traducciones
