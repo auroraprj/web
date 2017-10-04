13cc2d3c9a6764b0dda162a2b0750bdde9be91ef
Jose Carmona <j.carmona.n@gmail.com>
2017-10-04 18:45:27 +0200

    bloque de Investigaciones

--- tools/aurora_config_export.sh
+++ tools/aurora_config_export.sh
@@ -1,8 +1,8 @@
-#-- entorno
-. $HOME/web/tools/aurora_env.sh
+#-- configuración
+set -o allexport; source /aurora.conf; set +o allexport
 
 #-- operamos con drush
 cd $drupal
 
 #-- exportamos la configuración y añadimos a repositorio git
-drush config-export --add --destination=$git/config
+drush config-export --destination=/config
--- tools/aurora_env.sh
+++
@@ -1,5 +0,0 @@
-#-- entorno de ejecución de scripts
-export git=$HOME/web
-export drupal=$HOME/apps/drupal/htdocs
-export host='192.168.1.120'
-export admin=admin
